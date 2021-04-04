a7b457c9d86862f0f0a4922949be0068bb259a11
ezopezo <64305397+ezopezo@users.noreply.github.com>
2021-04-04 13:29:24 +0200

    change logic of tweet comparing (based on creation time), avoiding duplicite tweets

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import asyncio
 from datetime import datetime, timezone, date
 import pytz
 
-
 def set_api():
-    auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
-    auth.set_access_token(os.environ['ACCESS_KEY'], os.environ['ACCESS_SECRET'])
+    KEYS = list(open('keys.txt', 'r'))
+    auth = tweepy.OAuthHandler(KEYS[4].strip('\n'), KEYS[5].strip('\n')) # (os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+    auth.set_access_token(KEYS[6].strip('\n'), KEYS[7].strip('\n')) # (os.environ['ACCESS_KEY'], os.environ['ACCESS_SECRET'])
     api = tweepy.API(auth)
     return api
 
 
 def set_bot():
-    bot_token = os.environ['BOT_TOKEN']
-    chat_id = os.environ['CHAT_ID']
+    KEYS = list(open('keys.txt', 'r'))
+    bot_token = KEYS[2].strip('\n') #os.environ['BOT_TOKEN']
+    chat_id = KEYS[3].strip('\n') # os.environ['CHAT_ID']
     bot = telebot.TeleBot(bot_token, parse_mode=None)
     return bot, chat_id
 
@@ -38,31 +39,30 @@ def send_data_to_telegram(new_post):
         bot.send_message(chat_id, post)
 
 
-def identify_new_post(posts_queue):
-    last_post = posts_queue[-1]
+def identify_new_post(posts_queue, created_at_queue):
     tz = pytz.timezone('Europe/Paris')
     for post in obtain_data():
-        created_at_fixed_timezone = post.created_at.replace(tzinfo=timezone.utc).astimezone(tz)
-        actual_time = datetime.now(tz)
-        text_queue = {p[0] for p in posts_queue}
-        if post.text != last_post[0] and \
-            created_at_fixed_timezone.date() == actual_time.date() and \
+        creation_time = post.created_at.replace(tzinfo=timezone.utc).astimezone(tz)
+        process_time = datetime.now(tz)
+        if creation_time.date() == process_time.date() and \
             not '@' in post.text[0] and \
-            post.text not in text_queue:
+            creation_time not in created_at_queue:
             posts_queue.append((str(post.text), 
-                            created_at_fixed_timezone.strftime("%Y-%m-%d %H:%M:%S"),
-                            actual_time.strftime("%Y-%m-%d %H:%M:%S")))
+                            creation_time.strftime("%Y-%m-%d %H:%M:%S"),
+                            process_time.strftime("%Y-%m-%d %H:%M:%S")))
+            created_at_queue.append(creation_time)
             if len(posts_queue) > 10:
                 del posts_queue[0] 
+                del created_at_queue[0]
             return posts_queue[-1]
         else:
             return None
 
 
-async def control_node(wait_for, posts_queue):
+async def control_node(wait_for, posts_queue, created_at_queue):
     while True:
         await asyncio.sleep(wait_for)
-        new_post = identify_new_post(posts_queue)
+        new_post = identify_new_post(posts_queue, created_at_queue)
         if new_post:
             print('New record found: ', new_post)
             send_data_to_telegram(new_post)
@@ -73,8 +73,9 @@ async def control_node(wait_for, posts_queue):
 def main():
     while True:
         try:
+            created_at_queue = []
             posts_queue = [('text', 'created_at', 'timestamp')]
-            asyncio.run(control_node(2, posts_queue))
+            asyncio.run(control_node(2, posts_queue, created_at_queue))
         except Exception as e:
             print(e)
             bot, chat_id = set_bot()
