3e00e539ef501146b8072232e014457d6954f588
ezopezo <64305397+ezopezo@users.noreply.github.com>
2021-04-25 09:39:42 +0200

    rewritted to oop - avoid unnecessary recreating API connections, treated twitter-down edgecase (if post older than  minutes, not sending), removed bot restarting message

--- main.py
+++ main.py
@@ -6,104 +6,115 @@ from datetime import datetime, timezone, date, timedelta
 import pytz
 
 
-def set_api():
-    '''Setting twitter API'''
-    auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
-    auth.set_access_token(os.environ['ACCESS_KEY'], os.environ['ACCESS_SECRET'])
-    api = tweepy.API(auth)
-    return api
+class GetTweets:
+    def __init__(self):
+        self.twitter_api = None
+        self.telegram_bot = None
+        self.chat_id = None
+        self.created_at_queue = list() # custom queues for effective looking up, need lookup and ordering
+        self.posts_queue = list(('text', 'created_at', 'timestamp'))
 
 
-def set_bot():
-    '''Setting telegram bot'''
-    bot_token = os.environ['BOT_TOKEN']
-    chat_id = os.environ['CHAT_ID']
-    bot = telebot.TeleBot(bot_token, parse_mode=None)
-    return bot, chat_id
+    @property
+    def set_twitter_api(self):
+        '''Setting twitter API'''
+        auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+        auth.set_access_token(os.environ['ACCESS_KEY'], os.environ['ACCESS_SECRET'])
+        twitter_api = tweepy.API(auth)
+        return twitter_api
 
 
-def obtain_data():
-    '''Obtaining and yielding twitter post(s)'''
-    api = set_api()
-    post_collection = api.user_timeline("FantasyLabsNBA", count=1)
-    yield from post_collection
+    @property
+    def set_telegram_bot(self):
+        '''Setting telegram bot'''
+        bot_token = os.environ['BOT_TOKEN']
+        chat_id = os.environ['CHAT_ID']
+        telegram_bot = telebot.TeleBot(bot_token, parse_mode=None)
+        return telegram_bot, chat_id
 
 
-def parse_for_telegram(new_post):
-    '''Formating data for telegram'''
-    formated_post = new_post[0] + '\nCreated at: ' + new_post[1]
-    yield formated_post
+    def obtain_data(self):
+        '''Obtaining and yielding twitter post(s)'''
+        if self.twitter_api is None:
+            print('Setting twitter API connection...')
+            self.twitter_api = self.set_twitter_api
+        post_collection = self.twitter_api.user_timeline("FantasyLabsNBA", count=1, tweet_mode='extended')
+        yield from post_collection
 
 
-def send_data_to_telegram(new_post):
-    '''Sending data to telegram'''
-    bot, chat_id = set_bot()
-    for post in parse_for_telegram(new_post):
-        bot.send_message(chat_id, post)
+    def parse_for_telegram(self, new_post):
+        '''Formating data for telegram'''
+        formated_post = new_post[0] + '\nCreated at: ' + new_post[1]
+        yield formated_post
 
 
-def set_timestamps(post):
-    '''Adjusting correct timezone in created_at post attribute,
-    setting timestamp of post processing'''
-    tz = pytz.timezone('Europe/Paris')
-    process_time = datetime.now(tz)
-    creation_time = post.created_at.replace(tzinfo=timezone.utc).astimezone(tz).strftime("%Y-%m-%d %H:%M:%S")
-    return datetime.strptime(creation_time, "%Y-%m-%d %H:%M:%S"), process_time
+    def send_data_to_telegram(self, new_post):
+        '''Sending data to telegram'''
+        if self.telegram_bot is None or self.chat_id is None:
+            print('Setting Telegram bot...')
+            self.telegram_bot, self.chat_id = self.set_telegram_bot
+        for post in self.parse_for_telegram(new_post):
+            self.telegram_bot.send_message(self.chat_id, post)
+            print('Record sent', new_post, ', parsed to: ', post.replace('\n', ' '))
 
 
-def control_queues(posts_queue, created_at_queue, max_items_in_queue=20):
-    '''Controling queue of posts (and created_at queue timestamps) 
-    for effective memory management'''
-    if len(posts_queue) > max_items_in_queue:
-        del posts_queue[0] 
-        del created_at_queue[0]
+    def set_timestamps(self, post):
+        '''Adjusting correct timezone in created_at post attribute,
+        setting timestamp of post processing'''
+        tz = pytz.timezone('Europe/Paris')
+        process_time = datetime.now(tz)
+        creation_time = post.created_at.replace(tzinfo=timezone.utc).astimezone(tz).strftime("%Y-%m-%d %H:%M:%S")
+        return datetime.strptime(creation_time, "%Y-%m-%d %H:%M:%S"), process_time
 
 
-def identify_new_post(posts_queue, created_at_queue):
-    '''Identifying new post based on conditions:
-        1. Post from same day (avoiding different day artefacts)
-        2. Avoiding recieving comments of page itself (filtering posts with "@" in beginning)
-        3. Storing created_at time in custom queue and comparing every post to avoid duplicities 
-       Returning new post.'''
-    for post in obtain_data():
-        creation_time, process_time = set_timestamps(post)
-        if creation_time.date() == process_time.date() and \
-            not '@' in post.text[0] and \
-            creation_time not in created_at_queue and \
-            creation_time > datetime.now()-timedelta(seconds=7): # and if older than few seconds (to avoid repeating last tweet)
-
-            posts_queue.append((str(post.text), 
-                            creation_time.strftime("%Y-%m-%d %H:%M:%S"),
-                            process_time.strftime("%Y-%m-%d %H:%M:%S")))
-
-            created_at_queue.append(creation_time)
-            control_queues(posts_queue, created_at_queue)
-            return posts_queue[-1]
-        else:
-            return None
+    def maintain_queues(self, max_items_in_queue=20):
+        '''Controling queue of posts (and created_at queue timestamps) 
+        for effective memory management'''
+        if len(self.posts_queue) > max_items_in_queue:
+            del self.posts_queue[0] 
+            del self.created_at_queue[0]
 
 
-async def control_node(wait_for, posts_queue, created_at_queue):
-    '''If new past identified, it is logged and redirected for telegram processing '''
+    def identify_new_post(self):
+        '''Identifying new post based on conditions:
+            1. Avoiding recieving comments of page itself (filtering posts with "@" in beginning)
+            2. Storing created_at time in custom queue and comparing every post to avoid duplicities 
+            3. Not older than 5 minutes to avoid long-term spamming with last tweet in case of twitter down
+        Returning new post.'''
+        for post in self.obtain_data():
+            creation_time, processed_time = self.set_timestamps(post)
+            if not '@' in post.full_text[0] \
+                and creation_time not in self.created_at_queue \
+                and creation_time > datetime.now()-timedelta(seconds=500):
+
+                self.posts_queue.append((str(post.full_text), 
+                                creation_time.strftime("%Y-%m-%d %H:%M:%S"),
+                                processed_time.strftime("%Y-%m-%d %H:%M:%S")))
+
+                self.created_at_queue.append(creation_time)
+                self.maintain_queues()
+                return self.posts_queue[-1]
+            else:
+                return None
+
+
+
+async def control_node(wait_for_next_telegram_check):
+    '''If new post identified, it is passed for telegram processing '''
+    get_tweets = GetTweets()
     while True:
-        await asyncio.sleep(wait_for)
-        new_post = identify_new_post(posts_queue, created_at_queue)
+        await asyncio.sleep(wait_for_next_telegram_check)
+        new_post = get_tweets.identify_new_post()
         if new_post:
-            print('New record found: ', new_post)
-            send_data_to_telegram(new_post)
+            get_tweets.send_data_to_telegram(new_post)
 
 
 def main():
     while True:
         try:
-            created_at_queue = list() # custom queues for effective looking up
-            posts_queue = list(('text', 'created_at', 'timestamp'))
-            asyncio.run(control_node(wait_for=1.2, posts_queue=posts_queue, created_at_queue=created_at_queue))
+            asyncio.run(control_node(wait_for_next_telegram_check=1.2))
         except Exception as exc:
             print('Bot restarted: ', exc)
-            #bot, chat_id = set_bot()
-            print('Bot succesfully restarted... Last tweet may reappear.')
-            #bot.send_message(chat_id, 'Bot succesfully restarted... Last tweet may reappear.')
             continue
 
 
